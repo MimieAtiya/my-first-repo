ac49d9a304ec34c1028a35a85774167b52c56108
MimieAtiya <mimieatiya94@gmail.com>
2021-08-24 14:54:05 +0800

    Finish working with API

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+let url = ` https://restcountries.eu/rest/v2/name/Australia`
+
+function waitForData (res){
+    return res.json();
+}
+
+function handleData(data){
+    let pop = data[0].population;
+    console.log (` The population in Australia is ${pop}`);
+    //debugger; // give access to data
+}
+
+fetch(url, { method: "GET"}).then(waitForData).then(handleData);
